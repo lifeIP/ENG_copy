126cf65dd5bbf801c4fd84f174cd0b8888b69ecf
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-02-13 00:53:15 +0700

    The appearance of the copy method

--- C_Test.h
+++ C_Test.h
@@ -35,7 +35,7 @@ public:
 			file.open(fileName);
 		}
 		int counter_value = 0;
-		std::string s;
+		str s;
 		while (file.peek()!=EOF) {
 			std::getline(file,s);
 			counter_value++;
@@ -43,20 +43,29 @@ public:
 		closeFile(); 
 		return counter_value;
 	}
-	bool sorting() {
-		closeFile();
-		while (!file.is_open()) {
-			file.open(fileName);
+	void doCopyFile(str &file1, str endfile) {
+		std::fstream fileSource;
+		std::fstream fileFinite;
+		fileSource.open(file1);
+		fileFinite.open(endfile, std::ios::in | std::ios::app);
+		str copy_text;
+		while (fileSource.peek() != EOF) {
+			std::getline(fileSource, copy_text);
+			fileFinite << copy_text<<"\n";
 		}
+		fileSource.close();
+		fileFinite.close();
+	}
+	bool sorting() {
 		std::fstream sorter;
 		sorter.open(sortFile, std::ios::out |std::ios::trunc);
 		sorter.close();
 		while (!sorter.is_open()) {
 			sorter.open(sortFile, std::ios::in | std::ios::app );
 		} 
+		doCopyFile(fileName, sortFile);
 		
-		
-		closeFile();
+		sorter.close();
 		return true;
 	}
 
