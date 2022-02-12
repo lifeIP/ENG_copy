ea7b04fd5a14c25ea681c3858e61aeb291e76037
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-02-12 23:06:29 +0700

    Add files via upload

--- C_Test.h
+++ C_Test.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+typedef std::string str;
 
 class C_Test
 {
@@ -10,21 +11,36 @@ private:
 	std::string fileName = "File name";
 	std:: fstream file;
 public:
-	void set_fileName(std::string &FileName) {
+	void set_fileName(str &FileName) {
 		fileName = FileName;
 	}
-	void get_fileName(std::string &container) {
+	void get_fileName(str &container) {
 		container = fileName;
 	}
 	void openFile() {
-		file.open(fileName);
+		file.open(fileName, std::ios_base::out | std::ios_base::app);
 	}
 	bool get_status() const /* Константная функция - не может менять атрибуты класса. Только получать их */
 	{
 		return file.is_open();
 	}
-	void additional_recording() {
-		
+	void closeFile() {//Закрывает файл
+		file.close();
+	}
+	void additional_recording(str &add_str) {//Добавляет записи
+		file << add_str <<"\n";
+	}
+	int counter() {
+		closeFile();
+		file.open(fileName, std::ios::in);
+		int counter_value = 0;
+		std::string s;
+		while (!file.eof()) {
+			std::getline(file,s);
+			counter_value++;
+		}//Функция peek возвращает символ, который должен быть прочитан следующей функцией чтения потока. В частности, это позволяет распознать конец потока
+		closeFile(); 
+		return counter_value;
 	}
 
 	/* список методов доступных другим функциям и объектам программы */
--- test1.cpp
+++ test1.cpp
@@ -9,6 +9,11 @@ int main() {
 	H.set_fileName(FILEname);
 	H.get_fileName(H_answer);
 	H.openFile();
+	H.additional_recording(FILEname);
 	std::cout << H.get_status() << std::endl;
 	std::cout << H_answer;
+	int s;
+	s = H.counter();
+	std::cout << "\n\n" << s;
+
 }
